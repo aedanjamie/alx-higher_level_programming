0daf4cdbc5021f155026b818ae34017da24d2b31
aedanjamie <aedanjamie@gmail.com>
2022-12-07 02:18:32 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-        
+
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
