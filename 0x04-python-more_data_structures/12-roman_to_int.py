dc8d7fe47994e4c1bef577d4c095927045908c6e
aedanjamie <aedanjamie@gmail.com>
2022-12-09 02:32:06 -0800

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -26,7 +26,7 @@ def roman_to_int(roman_string):
 
     for ch in roman_string:
         for r_num in list_keys:
-            if r+num == ch:
+            if r_num == ch:
                 if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
                     list_num = [rom_n.get(ch)]
