4df6d38fc299846bfe39255d30c3f3f012b20d58
aedanjamie <aedanjamie@gmail.com>
2022-12-22 07:17:34 -0800

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,5 +14,6 @@ class Square:
             self.__size = size
 
     def area(self)
-    """Method that returns the square are of the object"""
+    """Method that returns the square are of the object
+    """
         return (self.__size ** 2)
