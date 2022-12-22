be74988db116b1fccaf9b2cbb660ead96ab0e36d
aedanjamie <106814782+aedanjamie@users.noreply.github.com>
2022-12-22 17:31:38 +0200

    Update 4-sqaure.py

--- 0x06-python-classes/4-sqaure.py
+++ 0x06-python-classes/4-sqaure.py
@@ -4,42 +4,36 @@
 
 
 class Square:
-    """A class that represents a square"""
-
+    """ A class that defines a square by its size
+    """
     def __init__(self, size=0):
-        """Initializing this square class
-        Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
+        """ Method to initialize the square object
         """
-
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-        self.__size = size
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
 
     @property
     def size(self):
-        """Retrieves size of square"""
-
+        """ Method to returns the size value
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Method to set the size value of the square object
+        """
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
-
-    def area(self):
-        """
-        Calculate area of the square
-        Returns: The square of the size
-        """
-
-        return (self.__size ** 2)
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
