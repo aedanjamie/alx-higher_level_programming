45b129530eeb0cde7c99255ac3e46e8cea099afa
aedanjamie <aedanjamie@gmail.com>
2023-01-10 05:56:10 -0800

    MyInt

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """Inherits int operators == and !="""
+
+    def _eq__(self. vlaue):
+        """Override == operator with != bahviour"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behaviour"""
+        return self.real == value
