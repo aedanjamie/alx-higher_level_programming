b9aabaad1a4fbd778f477add79e15e2ae0622b41
aedanjamie <aedanjamie@gmail.com>
2023-02-20 08:04:41 -0800

    SQL

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- displays the average temperature by city
 -- ordered by temperature desc
 
-SELECT cuty, AVG(value) as avg_temp
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
